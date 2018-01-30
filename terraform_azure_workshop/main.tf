84cb5b2cd066d893a6817c15bd119301a528544b
katsok9 <katsok9@gmail.com>
2018-01-30 23:11:48 +0200

    add module

--- terraform_azure_workshop/main.tf
+++ terraform_azure_workshop/main.tf
@@ -35,3 +35,15 @@ output "sb_address_prefix"
 output "vn_address_space" {
   value = "${azurerm_virtual_network.myfirstntwk.address_space}"
 }
+
+output "sb_id" {
+  value = "${azurerm_virtual_network.myfirstntwk.id}"
+}
+
+module "example" {
+  source              = "Azure/compute/azurerm"
+  location            = "${azurerm_resource_group.myfirstrg.location}"
+  vm_os_simple        = "UbuntuServer"
+  public_ip_dns       = ["akakadushbag0workshop"] // change to a unique name per datacenter region
+  vnet_subnet_id      = "${azurerm_virtual_network.myfirstntwk.id}"
+}
